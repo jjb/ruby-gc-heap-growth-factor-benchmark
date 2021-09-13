e17dcaf12d3b23876d638e3f2eeb20720df5e33e
John Bachir <j@jjb.cc>
2021-09-13 18:45:44 +0100

    purge disk cache between runs
    this seems to make an impact

--- runner.sh
+++ runner.sh
@@ -1,8 +1,15 @@
 bundle exec ruby code.rb
+sudo purge
 RUBY_GC_HEAP_GROWTH_FACTOR=2 bundle exec ruby code.rb
+sudo purge
 RUBY_GC_HEAP_GROWTH_FACTOR=1.8 bundle exec ruby code.rb
+sudo purge
 RUBY_GC_HEAP_GROWTH_FACTOR=1.6 bundle exec ruby code.rb
+sudo purge
 RUBY_GC_HEAP_GROWTH_FACTOR=1.4 bundle exec ruby code.rb
+sudo purge
 RUBY_GC_HEAP_GROWTH_FACTOR=1.2 bundle exec ruby code.rb
+sudo purge
 RUBY_GC_HEAP_GROWTH_FACTOR=1.1 bundle exec ruby code.rb
+# sudo purge
 # RUBY_GC_HEAP_GROWTH_FACTOR=1.01 bundle exec ruby code.rb
